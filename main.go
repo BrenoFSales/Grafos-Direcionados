54de08ef0694c1e6a215cde3689a4470cb7d546d
silva guimaraes <silva__guimaraes@hotmail.com>
2025-03-18 01:53:50 -0300

    Adicionar testes

--- main.go
+++ main.go
@@ -1,6 +1,102 @@
-package main;
-import  "fmt";
+package main
+
+import "fmt"
+
+type conjunto []*Node
+
+type Node struct {
+	conjunto conjunto
+	id       string
+	filhos   []*Node
+}
+
+// aresta unidirecional entre dois nós: a -> b.
+func (a *Node) Conectar(b *Node) {
+	panic("não implementado!")
+}
+
+// deve retornar o primeiro nó com o mesmo valor caso haja mais de um.
+func (a *Node) Get(valor string) (*Node, error) {
+	panic("não implementado!")
+}
+
+// grau de um vértice
+func (a *Node) Grau() int {
+	panic("não implementado!")
+}
+
+// deve remover o primeiro nó caso o haja mais de um.
+func (a *Node) Remover(b *Node) (*Node, error) {
+	panic("não implementado!")
+}
+
+// cria um novo nó sem conexões com tal valor.
+func (c conjunto) NovoNode(id string) *Node {
+	node := &Node{
+		conjunto: c,
+		id:       id,
+		filhos:   make([]*Node, 0),
+	}
+	c = append(c, node)
+	return node
+}
+
+// cria um novo nó filho pertencendo ao mesmo conjunto que o objeto sendo chamado.
+func (a *Node) NovoNode(id string) *Node {
+	node := a.conjunto.NovoNode(id)
+	a.Conectar(node)
+	return node
+}
+
+// cria um novo conjunto vazio de nós.
+func NovoConjunto() conjunto {
+	return make(conjunto, 0)
+}
+
+// cria uma matriz de adjacência onde todos os nós são ordenados pelo id em colunas e linhas
+// em ordem crescente
+func (c conjunto) MatrizAdjacencia() [][]int {
+	panic("não implementado!")
+}
+
+// cria uma lista de adjacência onde todos os conjuntos são representados pelos seus IDs, em ordem alfabética,
+// onde cada índice aponta para uma array dos nós filhos
+func (c conjunto) ListaAdjacencia() [][]*Node {
+	panic("não implementado!")
+}
+
+func (c conjunto) VerificarArvore() (GrafoArvore bool, ArvoreBinaria bool, ArvoreCheia bool, ArvoreCompleta bool) {
+	// não fica claro no documento quais tipos de árvores ele quer.
+	// talvez árvore binária seja redundante.
+	panic("não implementado!")
+}
+
+// retorna um map onde cada chave representa um nó do conjunto
+// e cada valor o número de grau da nó chave.
+func (c conjunto) VerticesGrau() map[*Node]int {
+	panic("não implementado!")
+}
+
+// retorna o número de grau máximo entre todos os vértices
+func (c conjunto) GrafoGrau() map[*Node]int {
+	panic("não implementado!")
+}
+
+// verifica se grafo é completo
+func (c conjunto) VerificarCompleto() bool {
+	panic("não implementado!")
+}
+
+// verifica se grafo possuí ao menos um vértice com um laço
+func (c conjunto) VerificarLacos() bool {
+	panic("não implementado!")
+}
+
+// verifica se grafo é um grafo simples
+func (c conjunto) VerificarSimples() bool {
+	panic("não implementado!")
+}
 
 func main() {
-	fmt.Println("Hello World!");
-} 
+	fmt.Println("Hello World!")
+}
---
+++ main_test.go
@@ -0,0 +1,332 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestNode(t *testing.T) {
+	conjunto := NovoConjunto()
+	a := conjunto.NovoNode("1")
+	b := conjunto.NovoNode("2")
+	c := conjunto.NovoNode("2")
+
+	a.Conectar(b)
+	a.Conectar(c)
+
+	resultado, err := a.Get("2")
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	esperado := b
+
+	if resultado != esperado {
+		t.Fatalf("Nó resultado não é o esperado. resultado: %p, esperado: %p", resultado, esperado)
+	}
+
+	a.Remover(b)
+
+	resultado, err = a.Get("2")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	esperado = c
+
+	if resultado != esperado {
+		t.Fatalf("Nó resultado não é o esperado. resultado: %p, esperado: %p", resultado, esperado)
+	}
+
+	a.Conectar(a)
+	a.Conectar(a)
+
+	a.Remover(a)
+
+	grauresultado := a.Grau()
+	grauEsperado := 1
+
+	if grauresultado != grauEsperado {
+		t.Fatalf("Grau nó resultado não é o esperado. resultado: %v, esperado: %p", resultado, esperado)
+	}
+}
+
+func TestConjunto(t *testing.T) {
+	conjunto := NovoConjunto()
+
+	var (
+		a = conjunto.NovoNode("1")
+		b = conjunto.NovoNode("2")
+		c = conjunto.NovoNode("3")
+		d = conjunto.NovoNode("4")
+		e = conjunto.NovoNode("5")
+		f = conjunto.NovoNode("6")
+	)
+
+	a.Conectar(b)
+	a.Conectar(c)
+	a.Conectar(d)
+	a.Conectar(e)
+	a.Conectar(f)
+
+	b.Conectar(c)
+	b.Conectar(d)
+	b.Conectar(e)
+	b.Conectar(f)
+
+	c.Conectar(d)
+	c.Conectar(e)
+	c.Conectar(f)
+
+	e.Conectar(a)
+
+	f.Conectar(e)
+	f.Conectar(f)
+	f.Conectar(b)
+
+	e.Conectar(a)
+
+	t.Run("MatrizAdjacencia", func(t *testing.T) {
+		resultado := conjunto.MatrizAdjacencia()
+
+		//   a  b  c  d  e  f
+		// a 0  1  1  1  1  1
+		// b 0  0  1  1  1  1
+		// c 0  0  0  1  1  1
+		// d 0  0  0  0  0  0
+		// e 2  0  0  0  0  0
+		// f 0  1  0  0  1  1
+		esperado := [][]int{
+			{0, 1, 1, 1, 1, 1},
+			{0, 0, 1, 1, 1, 1},
+			{0, 0, 0, 1, 1, 1},
+			{0, 0, 0, 0, 0, 0},
+			{2, 0, 0, 0, 0, 0},
+			{0, 1, 0, 0, 1, 1},
+		}
+
+		if !reflect.DeepEqual(resultado, esperado) {
+			t.Fatalf(
+				"matriz de adjacência retornada não é a esperada.\nresultado: %#v\nesperado: %#v",
+				resultado, esperado,
+			)
+		}
+	})
+	t.Run("listaAdjacencia", func(t *testing.T) {
+		resultado := conjunto.ListaAdjacencia()
+
+		esperado := [][]*Node{
+			{b, c, d, e, f},
+			{c, d, e, f},
+			{d, e, f},
+			{},
+			{a, a},
+			{e, f, b},
+		}
+
+		if !reflect.DeepEqual(resultado, esperado) {
+			t.Fatalf(
+				"lista de adjacência retornada não é a esperada.\nresultado: %#v\nesperado: %#v",
+				resultado, esperado,
+			)
+		}
+	})
+
+	t.Run("VerticesGrau", func(t *testing.T) {
+		resultado := conjunto.VerticesGrau()
+		esperado := map[*Node]int{a: 5, b: 4, c: 3, d: 0, e: 2, f: 3}
+
+		if !reflect.DeepEqual(resultado, esperado) {
+			t.Fatalf(
+				"lista de adjacência retornada não é a esperada.\nresultado: %#v\nesperado: %#v",
+				resultado, esperado,
+			)
+		}
+	})
+
+	t.Run("GrafoGrau", func(t *testing.T) {
+		resultado := conjunto.GrafoGrau()
+		esperado := 5
+
+		if !reflect.DeepEqual(resultado, esperado) {
+			t.Fatalf(
+				"lista de adjacência retornada não é a esperada.\nresultado: %#v\nesperado: %#v",
+				resultado, esperado,
+			)
+		}
+	})
+}
+
+func TestVerificarArvore(t *testing.T) {
+	conjunto := NovoConjunto()
+
+	var (
+		a = conjunto.NovoNode("1")
+		b = a.NovoNode("1")
+		c = a.NovoNode("1")
+
+		d = b.NovoNode("2")
+		e = b.NovoNode("2")
+
+		f = c.NovoNode("2")
+		g = c.NovoNode("2")
+
+		_ = d.NovoNode("3")
+		_ = d.NovoNode("3")
+		_ = e.NovoNode("3")
+		_ = e.NovoNode("3")
+
+		_ = f.NovoNode("3")
+		_ = f.NovoNode("3")
+		_ = g.NovoNode("3")
+		h = g.NovoNode("3")
+	)
+
+	arvore, binaria, cheia, completa := conjunto.VerificarArvore()
+	if !arvore {
+		t.Log("verificação retornada não é a esperada: grafo é uma árvore")
+		t.Fail()
+	}
+	if !binaria {
+		t.Log("verificação retornada não é a esperada: árvore é binária")
+		t.Fail()
+	}
+	if !cheia {
+		t.Log("verificação retornada não é a esperada: árvore é cheia")
+		t.Fail()
+	}
+	if completa {
+		t.Fatal("verificação retornada não é a esperada: árvore não é completa")
+	}
+	if t.Failed() {
+		t.FailNow()
+	}
+
+	g.Remover(h)
+	arvore, binaria, cheia, completa = conjunto.VerificarArvore()
+	if !arvore {
+		t.Log("verificação retornada não é a esperada: grafo é uma árvore")
+		t.Fail()
+	}
+	if !binaria {
+		t.Log("verificação retornada não é a esperada: árvore é binária")
+		t.Fail()
+	}
+	if cheia {
+		t.Log("verificação retornada não é a esperada: árvore não é mais cheia")
+		t.Fail()
+	}
+	if !completa {
+		t.Log("verificação retornada não é a esperada: árvore agora é completa")
+		t.Fail()
+	}
+	if t.Failed() {
+		t.FailNow()
+	}
+
+	a.Remover(b)
+	arvore, binaria, cheia, completa = conjunto.VerificarArvore()
+	if !arvore {
+		t.Log("verificação retornada não é a esperada: grafo é uma árvore")
+		t.Fail()
+	}
+	if !binaria {
+		t.Log("verificação retornada não é a esperada: árvore é binária")
+		t.Fail()
+	}
+	if cheia {
+		t.Log("verificação retornada não é a esperada: árvore ainda é cheia")
+		t.Fail()
+	}
+	if completa {
+		t.Log("verificação retornada não é a esperada: árvore agora não mais é completa")
+		t.Fail()
+	}
+	if t.Failed() {
+		t.FailNow()
+	}
+}
+
+func TestVerificarCompleto(t *testing.T) {
+
+	conjunto := NovoConjunto()
+	vertices := []*Node{
+		conjunto.NovoNode("1"), conjunto.NovoNode("2"), conjunto.NovoNode("3"),
+		conjunto.NovoNode("4"), conjunto.NovoNode("5"), conjunto.NovoNode("6"),
+		conjunto.NovoNode("7"),
+	}
+	for i := range vertices {
+		for j := 0; j+i < len(vertices); j++ {
+			a, b := vertices[i], vertices[j]
+			a.Conectar(b)
+			b.Conectar(a)
+		}
+	}
+	resultado := conjunto.VerificarCompleto()
+	esperado := true
+
+	if resultado != esperado {
+		t.Fatal("verificação retornada não é a esperada: grafo é completo")
+	}
+}
+
+func TestVerificarLacos(t *testing.T) {
+	conjunto := NovoConjunto()
+
+	a := conjunto.NovoNode("1")
+	a.Conectar(a)
+
+	resultado := conjunto.VerificarLacos()
+	esperado := true
+
+	if resultado != esperado {
+		t.Fatal("verificação retornada não é a esperada: grafo é possuí um laço")
+	}
+
+	conjunto = NovoConjunto()
+	conjunto.NovoNode("1").NovoNode("1")
+
+	resultado = conjunto.VerificarLacos()
+	esperado = false
+
+	if resultado != esperado {
+		t.Fatal("verificação retornada não é a esperada: segundo grafo não possuí um laços")
+	}
+
+}
+
+func TestVerificarSimples(t *testing.T) {
+	conjunto := NovoConjunto()
+
+	a := conjunto.NovoNode("1")
+	b := a.NovoNode("2")
+	a.Conectar(b)
+
+	resultado := conjunto.VerificarSimples()
+	esperado := false
+
+	if resultado != esperado {
+		t.Fatal("verificação retornada não é a esperada: grafo não é simples")
+	}
+
+	conjunto = NovoConjunto()
+
+	a = conjunto.NovoNode("1")
+	b = a.NovoNode("2")
+	c := b.NovoNode("3")
+	c.Conectar(a)
+	a.Conectar(a)
+
+	resultado = conjunto.VerificarSimples()
+	esperado = true
+
+	if resultado != esperado {
+		t.Fatal("verificação retornada não é a esperada: grafo é simples")
+	}
+
+}
+
+func TestImprimir(t *testing.T) {
+	// sono....
+}
